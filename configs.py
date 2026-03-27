d336282d563cb80a26a286d7bb96ed5a85be777c
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-27 10:50:55 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,9 +1,8 @@
 import os
 
 class cfg:
-    API_ID = int(os.getenv("API_ID", "1234567"))
-    API_HASH = os.getenv("API_HASH", "your_api_hash")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
-    API = os.getenv("API", "IN")
-    MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_url")
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGO_URI = os.getenv("MONGO_URI")
+    OWNER_ID = int(os.getenv("OWNER_ID"))
