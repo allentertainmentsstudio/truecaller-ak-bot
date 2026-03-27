ab1d6368025242180c2140f4611cd2819aecf358
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-27 11:29:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,8 +1,11 @@
-import os
+from os import getenv
 
-class cfg:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    MONGO_URI = os.getenv("MONGO_URI")
-    OWNER_ID = int(os.getenv("OWNER_ID"))
+class Config:
+    API_ID = int(getenv("API_ID"))
+    API_HASH = getenv("API_HASH")
+    BOT_TOKEN = getenv("BOT_TOKEN")
+    MONGO_URI = getenv("MONGO_URI")
+    OWNER_ID = int(getenv("OWNER_ID"))
+    UPI_ID = getenv("UPI_ID", "yourupi@upi")
+
+cfg = Config()
