50717ee68b846ed6f8fe8b018ea6331733b97121
allentertainmentsstudio <allentertainmentsstudio@gmail.com>
2026-03-27 12:04:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN")
     MONGO_URI = getenv("MONGO_URI")
     OWNER_ID = int(getenv("OWNER_ID"))
-    UPI_ID = getenv("UPI_ID", "yourupi@upi")
-
+    UPI_ID = getenv("UPI_ID", "971916880@ybl")
+    # ✅ FIXED
+    API_KEY = getenv("API_KEY", "41ecb7c71fe62512fad162c8800fc85e")
+    
 cfg = Config()
